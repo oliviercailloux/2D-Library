21d7e56ae0cf484b50319536ff2f04a2b52ec00a
eliechartouni <eliechartouny11@hotmail.com>
2017-05-11 11:25:21 +0200

    Update readFile.java

--- src/main/java/controler/readFile.java
+++ src/main/java/controler/readFile.java
@@ -7,9 +7,54 @@ import java.util.List;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import model.Author;
 import model.Book;
 
+/**
+
+
+public class CSVReader {
+
+    public static void main(String[] args) {
+
+        String csvFile = "/Users/mkyong/csv/country.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] country = line.split(cvsSplitBy);
+
+                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+}
+ */
 public class readFile {
 	
 	/**
