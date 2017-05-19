b497c3e73e7c4b687d858ed40f7bd10503664158
suquol13 <olympie.suquet@laposte.net>
2017-05-19 16:08:01 +0200

    Mise à jour du Main principal : - lecture du fichier - génération d'un bibliothèque - lancement de l'interface graphique

--- src/main/java/io/github/dauphine/lejema160/main/Main.java
+++ src/main/java/io/github/dauphine/lejema160/main/Main.java
@@ -16,19 +16,36 @@ import io.github.dauphine.lejema160.model.Library;
 import io.github.dauphine.lejema160.model.Shelf;
 import io.github.dauphine.lejema160.model.User;
 import io.github.dauphine.lejema160.view.SVGDrawable;
+import io.github.dauphine.lejema160.view.Svg2jpg;
+import io.github.dauphine.lejema160.view.Window2DLibrary;
 
 public class Main {
 	
 	public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) throws IOException, ParserConfigurationException {
-	
+		
+		// lire le fichier afin de créer une liste de livres.
 		List<Book> books = io.github.dauphine.lejema160.controller.readFile.read();
 		//Library Lib = io.github.dauphine.lejema160.model.User.createLibrary(books, 50, 70);
+		
+		// créer une librairie à partir de la liste de livres.
 		Library Lib = new Library(books);
-		io.github.dauphine.lejema160.view.SVGDrawable.generate(Lib, true);
-		System.out.println("Fait");
-
+		boolean leaning = true;
+		
+		
+		io.github.dauphine.lejema160.view.SVGDrawable.generate(Lib, leaning);
+		//System.out.println("Fait");
+		
+		
+		// lancement de l'interface graphique
+		try {
+			Svg2jpg.convert();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		new Window2DLibrary("2D_LIBRARY PROJECT");
 		
 	}
 
