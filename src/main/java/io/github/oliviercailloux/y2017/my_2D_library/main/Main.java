58e5d7c18155913e51572fe4b06529215fa22c41
lejema160 <merlene.lejeune@outlook.com>
2017-06-18 23:24:26 +0200

    debug sort premiere librairie

--- src/main/java/io/github/oliviercailloux/y2017/my_2D_library/main/Main.java
+++ src/main/java/io/github/oliviercailloux/y2017/my_2D_library/main/Main.java
@@ -6,6 +6,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.github.oliviercailloux.y2017.my_2D_library.controller.DataFile;
+import io.github.oliviercailloux.y2017.my_2D_library.model.Library;
 import io.github.oliviercailloux.y2017.my_2D_library.view.SVGLibrary;
 import io.github.oliviercailloux.y2017.my_2D_library.view.Window2DLibrary;
 
@@ -14,7 +16,9 @@ public class Main {
 	public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) throws IOException, ParserConfigurationException {
-		SVGLibrary svgLibrary = new SVGLibrary();	
+		DataFile dataFile = new DataFile();
+		Library library = new Library(dataFile.read(), 18);
+		SVGLibrary svgLibrary = new SVGLibrary(library );	
 		new Window2DLibrary("2D_LIBRARY PROJECT", svgLibrary);
 	}
 
